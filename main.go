ba109cfa30c1b474959630dba7737b106fe99b19
Tangyonghai <tech@geiqin.com>
2020-12-07 09:24:22 +0800

    111
    11

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/geiqin/microkit/kubernetes/config"
 	k8s "github.com/geiqin/microkit/kubernetes/micro"
 	"github.com/micro/go-micro/v2"
 	"log"
@@ -19,7 +18,6 @@ func main() {
 	service.Init()
 
 
-	config.NewConfig()
 
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
