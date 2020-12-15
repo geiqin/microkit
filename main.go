0adba11f0c54a3d09272dd1795b9d651687c4f37
Tangyonghai <tech@geiqin.com>
2020-12-15 15:22:27 +0800

    11
    111

--- main.go
+++ main.go
@@ -1,25 +1,9 @@
 package main
 
-import (
-	k8s "github.com/geiqin/microkit/kubernetes/micro"
-	"github.com/micro/go-micro/v2"
-	"log"
-)
 
 type Say struct{}
 
 
 
 func main() {
-	service := k8s.NewService(
-		micro.Name("greeter"),
-	)
-
-	service.Init()
-
-
-
-	if err := service.Run(); err != nil {
-		log.Fatal(err)
-	}
 }
