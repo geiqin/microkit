6d0bccfad22bc4a6fdf2186e35b51f89b1957c4b
Tangyonghai <tech@geiqin.com>
2020-12-19 10:22:12 +0800

    Update models.go

--- gateway/models.go
+++ gateway/models.go
@@ -9,12 +9,12 @@ type GrantChanel struct {
 
 //授权路由
 type GrantRoute struct {
-	Id          int    `json:"id"`                            //id
-	Name        string `json:"name"`                          //名称
-	Path        string `json:"path"`                          //路径
-	Method      string `json:"method"`                        //请求方式 ALL GET POST DELETE
-	HasStore    bool   `json:"has_store" gorm:"default:0"`    //授权店铺
-	HasUser     bool   `json:"has_user" gorm:"default:0"`     //授权用户
-	HasCustomer bool   `json:"has_customer" gorm:"default:0"` //授权客户
-	Unlimited   bool   `json:"unlimited" gorm:"default:1"`    //无限制（任何人都可以访问）
+	Id            int    `json:"id"`                            //id
+	Name          string `json:"name"`                          //名称
+	Path          string `json:"path"`                          //路径
+	Method        string `json:"method"`                        //请求方式 ALL GET POST DELETE
+	HasStore      bool   `json:"has_store" gorm:"default:0"`    //是否店铺API
+	AllowUser     bool   `json:"has_user" gorm:"default:0"`     //授权用户
+	AllowCustomer bool   `json:"has_customer" gorm:"default:0"` //授权客户
+	Unlimited     bool   `json:"unlimited" gorm:"default:1"`    //无限制（任何人都可以访问）
 }
