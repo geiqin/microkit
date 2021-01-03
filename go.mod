57f088a457d49494723f9bb8b14863fb4695094a
Tangyonghai <tech@geiqin.com>
2021-01-03 16:03:14 +0800

    11
    11

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/golang/protobuf v1.4.0
 	github.com/micro/go-micro/v2 v2.9.1
+	github.com/micro/go-plugins/registry/nats/v2 v2.9.1 // indirect
 	google.golang.org/protobuf v1.23.0
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.8
--- pubsub/pubSub.go
+++ pubsub/pubSub.go
@@ -3,20 +3,37 @@ package pubsub
 import (
 	"github.com/geiqin/gotools/helper"
 	"github.com/micro/go-micro/v2/broker"
+	"github.com/micro/go-micro/v2/broker/nats"
+	_ "github.com/micro/go-plugins/registry/nats/v2"
 	"log"
+	"os"
 )
+
+var myBroker broker.Broker
 
 type EventType string
 
 //消息注册
 func Register() {
-	if err := broker.Init(); err != nil {
-		log.Fatalf("Broker Init error: %v", err)
+
+	myBroker = nats.NewBroker(broker.Addrs(os.Getenv("MICRO_BROKER_ADDRESS")))
+	myBroker.Init()
+	err := myBroker.Connect()
+	if err != nil {
+		log.Println("broker connect error: %v", err)
 	}
 
-	if err2 := broker.Connect(); err2 != nil {
-		log.Fatalf("Broker Connect error: %v", err2)
-	}
+	log.Println("broker address:", myBroker.Address())
+	/*
+		if err := broker.Init(); err != nil {
+			log.Fatalf("Broker Init error: %v", err)
+		}
+
+		if err2 := broker.Connect(); err2 != nil {
+			log.Fatalf("Broker Connect error: %v", err2)
+		}
+
+	*/
 }
 
 //消息发布
@@ -36,12 +53,12 @@ func Publish(eventName EventType, storeId int64, data string, headers ...map[str
 		Header: heads,
 		Body:   []byte(data),
 	}
-	err := broker.Publish(string(eventName), msg)
+	err := myBroker.Publish(string(eventName), msg)
 	return err
 }
 
 //订阅消息
 func Subscribe(eventName EventType, handler broker.Handler) (broker.Subscriber, error) {
-	sub, err := broker.Subscribe(string(eventName), handler)
+	sub, err := myBroker.Subscribe(string(eventName), handler)
 	return sub, err
 }
