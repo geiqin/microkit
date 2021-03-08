82f13992f914258409bad8bbc28d69856dea68cf
Tangyonghai <tech@geiqin.com>
2021-03-08 15:04:44 +0800

    update connectDb

--- database/dba.go
+++ database/dba.go
@@ -10,15 +10,19 @@ import (
 var mu sync.Mutex
 
 //连接数据库（包括公共库和私有库）
-func ConnectDB(ctx context.Context, appFlag string, appPrivate bool) *gorm.DB {
+func ConnectDB(ctx context.Context, appFlag string, appPrivate bool, storeDbPrefix ...string) *gorm.DB {
 	mu.Lock()
 	defer mu.Unlock()
 	cfg := *GetConnectCfg(appFlag)
 
 	if appPrivate {
+		p := "go_store_"
+		if storeDbPrefix != nil {
+			p = storeDbPrefix[0]
+		}
 		storeId := auth.GetStoreId(ctx)
 		if storeId > 0 {
-			cfg.Database = auth.GetStoreFlag(storeId)
+			cfg.Database = auth.GetStoreFlag(storeId, p)
 		}
 	}
 	return DbPools(&cfg)
